4324197918b59d1e410d144001572b7ad27e948d
Joao Pedro <joaopedropavanelo@gmail.com>
2026-06-21 15:43:23 -0300

    Começo frontend

---
+++ frontend/js/login.js
@@ -0,0 +1,7 @@
+const formLogin = document.getElementById("form-login");
+
+formLogin.addEventListener("submit", function(event) ) {
+    event.preventDefault();
+
+    console.log("Formulario Enviado!")
+}
